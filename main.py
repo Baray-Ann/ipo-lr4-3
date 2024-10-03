ca49fe9c91a43b9c449d5f992c23f915616b1a06
Baray Ann <annbaray66@gmail.com>
2024-10-03 10:09:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+n=int(input("Введите число, до которого будут выводиться числа Фибоначчи: "))
+print("Ваше число:", n)
+fsum=0
+f1=0
+f2=1
+fsum=0
+while fsum<=n:
+    print(fsum)
+    fsum=f1+f2
+    f1=f2
+    f2=fsum
